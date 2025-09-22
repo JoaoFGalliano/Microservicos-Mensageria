95327bc6e05e36acfe617d876d0ef57eeaf2bfa5
JoaoFGalliano <gallianojoao@gmail.com>
2025-09-22 18:51:14 -0300

    Controller, Service, RecordDTO

--- user/src/main/java/com/ms/user/controller/UserController.java
+++ user/src/main/java/com/ms/user/controller/UserController.java
@@ -1,8 +1,30 @@
 package com.ms.user.controller;
-
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.ms.user.dtos.UserRecordDTO;
+import com.ms.user.models.UserModel;
+import com.ms.user.service.UserService;
+import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
+
+	final UserService userService;
+	
+public UserController(UserService userService) {
+	this.userService = userService;
+}
+	
+@PostMapping("/users")
+public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO) {
+
+	var userModel = new UserModel();
+	BeanUtils.copyProperties(userRecordDTO, userModel);
+	return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
+}
 
 }
